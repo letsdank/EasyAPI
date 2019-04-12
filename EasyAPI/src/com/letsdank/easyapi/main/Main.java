ad29a6921c61d85b159c41e3d54e27c29ee7e675
LetsDank <ichi2209@icloud.com>
2019-04-12 20:55:37 +0300

    Updated. lalalala
    Signed-off-by: LetsDank <ichi2209@icloud.com>

--- EasyAPI/src/com/letsdank/easyapi/main/Main.java
+++ EasyAPI/src/com/letsdank/easyapi/main/Main.java
@@ -3,9 +3,16 @@ package com.letsdank.easyapi.main;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+	
+	public static final String VERSION = "0.1";
+	
+	public static JavaPlugin instance;
+	
 	@Override
 	public void onEnable() {
-		System.out.println("Hello!");
+		
+		instance = this;
+		
+		System.out.println("Initialized EasyAPI version " + VERSION);
 	}
 }
