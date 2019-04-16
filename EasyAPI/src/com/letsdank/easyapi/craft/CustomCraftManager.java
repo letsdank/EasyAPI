e1eea58d79211d79dae7ddf8787cf73375260c7b
LetsDank <ichi2209@icloud.com>
2019-04-16 23:04:39 +0300

    Custom Craft Part 1
    Signed-off-by: LetsDank <ichi2209@icloud.com>

--- EasyAPI/src/com/letsdank/easyapi/craft/CustomCraftManager.java
+++ EasyAPI/src/com/letsdank/easyapi/craft/CustomCraftManager.java
@@ -13,22 +13,76 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.letsdank.easyapi.craft;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import com.google.common.collect.Lists;
+import com.letsdank.easyapi.main.Main;
 
 /**
  * 
  */
 public class CustomCraftManager {
 					 /* id */
+	public static boolean INITIALIZED = false;
 	public static Map<String, CraftRecipe> customCrafts = new HashMap<String, CraftRecipe>();
+	
+	public static void initialize() {
+		INITIALIZED = true;
+		
+		//
+		// Добавляем слушатель, который будет смотреть, какие предметы поместил игрок в
+		// стол крафта, затем, мы будем проверять, совпадает ли крафт с тем, что поставил
+		// игрок.
+		//
+		
+		Bukkit.getPluginManager().registerEvents(new Listener() {
+			
+			@EventHandler
+			public void onCraft(InventoryMoveItemEvent e) {
+				
+				//
+				// Проверка, что это верстак.
+				// TODO: Сделать то же самое с верстаком игрока.
+				//
+				
+				if (e.getDestination().getType() == InventoryType.WORKBENCH) {
+					
+					for (Map.Entry<String, CraftRecipe> entry : customCrafts.entrySet()) {
+						Inventory inv = entry.getValue().toWorkbenchTableInventory();
+						
+						ItemStack[] s1 = inv.getContents();
+						ItemStack[] s2 = e.getDestination().getContents();
+						
+						for (int i = 0; i < s1.length - 1; i++) {
+							if (!(s1[i].getType() == s2[i].getType())) {
+								return;
+							}
+						}
+						
+						//
+						// Все совпадает.
+						//
+						
+						e.getDestination().setItem(10, entry.getValue().getResult());
+					}
+					
+				}
+			}
+		}, Main.instance);
+	}
 	
 	public static CraftRecipe getCraftRecipe(String id) {
 		for (Map.Entry<String, CraftRecipe> entry : customCrafts.entrySet()) {
@@ -48,7 +102,7 @@ public class CustomCraftManager {
 		
 		for (Map.Entry<String, CraftRecipe> entry : customCrafts.entrySet()) {
 			if (newId.equalsIgnoreCase(entry.getKey())) {
-				System.err.println("ERROR: Recipe " + newId + " is already exists!");
+				System.err.println("ERROR: Recipe " + newId + " is already exist!");
 				return;
 			}
 		}
@@ -57,6 +111,9 @@ public class CustomCraftManager {
 	}
 	
 	public class CraftRecipe {
+		
+		private static final String CRAFT_PATTERN = "ABCDEFGHI";
+		
 		private String firstRow;
 		private String secondRow;
 		private String thirdRow;
@@ -121,6 +178,19 @@ public class CustomCraftManager {
 		 */
 		public List<ItemStack> getStacks() {
 			return stacks;
+		}
+		
+		public Inventory toWorkbenchTableInventory() {
+			Inventory inv = Bukkit.createInventory(null, InventoryType.WORKBENCH);
+			String s = firstRow + secondRow + thirdRow;
+			char[] ss = s.toCharArray();
+			
+			for (int i = 0; i < ss.length; i++) {
+				int index = CRAFT_PATTERN.indexOf(ss[i]);
+				inv.setItem(i, stacks.get(index - 1));
+			}
+			
+			return inv;
 		}
 		
 		/**
