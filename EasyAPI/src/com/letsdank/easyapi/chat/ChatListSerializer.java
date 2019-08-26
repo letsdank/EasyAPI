553006979f068ed145ef10297a5eda05f63a0a09
LetsDank <ichi2209@icloud.com>
2019-08-26 15:39:08 +0300

    part 2

---
+++ EasyAPI/src/com/letsdank/easyapi/chat/ChatListSerializer.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2019 LetsDank.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.letsdank.easyapi.chat;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+/**
+ * 
+ */
+public class ChatListSerializer {
+	
+	public List<TextComponent> serialize(File file, String startPos) {
+		return serialize(file, startPos, false);
+	}
+	
+	List<TextComponent> serialize(File file, String startPos, boolean extra) {
+		YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+		ConfigurationSection section = startPos != null && startPos != "" ?
+				config.getConfigurationSection(startPos) : config;
+		
+		if (!section.isConfigurationSection("chatlist")) {
+			System.out.println("Cannot find chatlist, returned null");
+			return null;
+		}
+		
+		section = section.getConfigurationSection("chatlist");
+		
+		List<TextComponent> result = new ArrayList<TextComponent>();
+		
+		for (Map.Entry<String, Object> entry : section.getValues(false).entrySet()) {
+			if (!section.isConfigurationSection(entry.getKey())) {
+				System.out.println("This list should contain only sections, returned null");
+				return null;
+			}
+			
+			ConfigurationSection entrySection = section.getConfigurationSection(entry.getKey());
+			
+			result.add(new ChatSerializer().serialize(file, entrySection.getCurrentPath(), extra));
+		}
+		
+		return result;
+	}
+}
